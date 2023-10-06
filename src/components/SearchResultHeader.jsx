b36bccdb548ebd623afb0173e33522431464e3ea
rohit-kumar-dev <ssrohitmechengg@gmail.com>
2023-10-06 19:15:41 +0530

    Search Result   header menu ' ALL IMAGE NEWS VIDEO SHOPPING'

--- src/components/SearchResultHeader.jsx
+++ src/components/SearchResultHeader.jsx
@@ -8,7 +8,33 @@ import { Context } from "../utils/ContextApi";
 import { menu } from "../utils/Constants";
 
 const SearchResultHeader = () => {
-    return <div>Search result header</div>;
+  const clickHandler = () => {};
+  return (
+    <div className="p-[15px] pb-0 md:pr-5 md:pl-20 md:pt-7 border-b border-[#ebebeb] flex md:block flex-col items-center sticky top-0 bg-white">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center grow">
+          <img className="hidden md:block w-[92px] mr-10" src={Logo} />
+          <SearchInput from="searchResult" />
+          ``
+        </div>
+        <div className="hidden md:block">
+          <ProfileIcon />
+        </div>
+      </div>
+      <div className="flex ml-[-12px] mt-3">
+        {menu.map((menu, index) => (
+          <span
+            key={index}
+            className={`flex items-center p-3 text-[#5f6368] cursor-pointer `}
+            onClick={() => useLinkClickHandler(menu)}
+          >
+            <span className="hidden md:block mr-2">{menu.icon}</span>
+            <span className="text-sm">{menu.name}</span>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchResultHeader;
