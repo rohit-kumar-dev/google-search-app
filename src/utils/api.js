83ffaa161b21b5d9840a42ed850f1b846d8fc68d
rohit-kumar-dev <ssrohitmechengg@gmail.com>
2023-10-08 13:37:18 +0530

    API intergration & Axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyD8TkOejZlBbudgAf7v-4zuXzH_lSdSNnk",
+  cx: "5678ff6e382b14142",
+};
+
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
